e26e12a1fdb8942afa73f9489b9ae24f54a1f91a
birdtakao <toriyamatakao@toriyamatakaos-MacBook-Pro.local>
2017-10-04 21:46:40 +0900

    Add before action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,12 @@
 class MessagesController < ApplicationController
+
+  before_action :move_to_index, except: :index
+
   def index
+  end
+
+  private
+  def move_to_index
+    redirect_to action: :index
   end
 end
