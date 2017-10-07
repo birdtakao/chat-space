4c3515f6ee5e259c0f54401b49b3e0e8f1eda322
birdtakao <toriyamatakao@toriyamatakaos-MacBook-Pro.local>
2017-10-07 13:31:16 +0900

    Edit routes setting

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'group#index'
 
-  resources :users, only: [:index]
+  resources :users, only: [:index, :edit]
+  resources :group, only: [:index, :new, :edit]
 end
