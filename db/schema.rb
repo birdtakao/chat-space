0a1c496025ee6635f3842e912b198d7c9794457a
birdtakao <toriyamatakao@toriyamatakaos-MacBook-Pro.local>
2017-10-15 15:31:55 +0900

    Create a Membership Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171015053757) do
+ActiveRecord::Schema.define(version: 20171015062908) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.text     "name",       limit: 65535
+  end
+
+  create_table "memberships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "group_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
