fe187705e7186c0fa8416d8ecca3c1e5691deaf5
birdtakao <toriyamatakao@toriyamatakaos-MacBook-Pro.local>
2017-10-15 19:32:22 +0900

    edit groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
-      render new_group_path, notice: 'グループを作成に失敗しました'
+      render new_group_path
     end
   end
 
@@ -20,6 +20,6 @@ class GroupsController < ApplicationController
 
   private
   def group_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, user_id: [])
   end
 end
