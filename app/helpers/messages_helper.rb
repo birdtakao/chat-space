bb4cfd701db5e5ab02c531a13dcf9ef2cd86af61
birdtakao <toriyamatakao@toriyamatakaos-MacBook-Pro.local>
2017-10-28 19:16:06 +0900

    Create a helper method

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,14 @@
 module MessagesHelper
+  def message_if(group)
+    if group.messages.blank?
+      "まだメッセージはありません。"
+    else
+      latest = group.messages.order("created_at DESC")[0]
+       if latest.text.blank?
+         "画像が投稿されています"
+       else
+        latest.text
+      end
+    end
+  end
 end
