f763bcefae6181e1afab38d1a9ca729a8c6d954e
birdtakao <toriyamatakao@toriyamatakaos-MacBook-Pro.local>
2017-11-12 18:59:14 +0900

    Set factories

--- spec/factories/group.rb
+++ spec/factories/group.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :group do
-     id         1
-     name      Faker::StarWars.planet
+     name       { Faker::Team.name }
+     created_at { Faker::Time.between(2.days.ago, Time.now, :all) }
+     updated_at { Faker::Time.between(2.days.ago, 1.days.ago, :all) }
   end
 end
